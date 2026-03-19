80513f37728c211cc1ebad6cd1b142f5d59517a3
hetari <hetari4py@gmail.com>
2026-03-20 02:47:41 +0300

    chore: add all shadcn-vue component and try a test component with the registry

--- packages/registry/default/hello-world/HelloWorld.vue
+++ packages/registry/default/hello-world/HelloWorld.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@sulaf/ui/components/ui/button'
+</script>
 
 <template>
   <div>Hello</div>
+  <Button>World from sulaf</Button>
 </template>
-
-<style lang="scss" scoped></style>
